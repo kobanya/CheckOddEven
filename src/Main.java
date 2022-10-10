2e81a082daadfa0d077f6de8ad552bd924d90e0b
kobanya <nagy.bela.budapest@gmail.com>
2022-10-10 11:41:53 +0200

    A kód feltöltése

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+public class Main {   // Save as "CheckOddEven.java"
+    public static void main(String[] args) {  // Program entry point
+        int number = 20;       // Set the value of "number" here!
+        System.out.println("The number is " + number);
+        if (number % 2== 0) {
+            System.out.println( "Evan number / páros szám");   // even number
+        } else {
+            System.out.println( "odd number / páratlan szám");   // odd number
+        }
+        System.out.println( "bye / Szia"); }
 }
